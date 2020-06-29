3809604861fae718e6ce37e9b04e5e2372575eb4
Vladimir <venom3333@gmail.com>
2020-06-29 11:02:28 +0300

    Contracts project

---
+++ CPK.Contracts/ConfigModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CPK.Contracts
+{
+    public class ConfigModel
+    {
+        public string SsoUri { get; set; } = string.Empty;
+        public string ApiUri { get; set; } = string.Empty;
+    }
+}
